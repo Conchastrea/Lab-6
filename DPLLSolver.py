abb902c1dc729b94c37f7ea12d4f922140abec90
Conchastrea <legopikachu1337@gmail.com>
2026-04-28 17:17:52 -0500

    Hello

--- DPLLSolver.py
+++ DPLLSolver.py
@@ -11,70 +11,68 @@ import Formula
 import datetime
 
 def theLoop(formula, branch):
-    # creates a copy for testing, allows formula to be for backtracking
-    newFormula = Formula.Formula()
-    newFormula.clauses = formula.clauses.copy()
-    newFormula.model = formula.model.copy()
-    newFormula.n = formula.n
-    newFormula.m = formula.m
 
     # print()
-    # print("Start", "newFormula", newFormula.model, "Formula", formula.model)
+    # print("Start", "formula", formula.model, "Formula", formula.model)
 
     # check for tautology and contradiction
-    if newFormula.istrue() == True:
+    if formula.istrue() == True:
         # print("Taut True")
-        return True, newFormula.model, branch
-    elif newFormula.isfalse() == True:
+        return True, formula.model, branch
+    elif formula.isfalse() == True:
         # print("Contra False")
-        return False, newFormula.model, branch
+        return False, formula.model, branch
 
-    # check for units
-    unit = newFormula.unit()
-    while unit != None:
-        # print("unit", unit)
-        newFormula.assign(unit)
-        if newFormula.istrue() == True:
-            # print("Unit True")
-            return True, newFormula.model, branch
-        elif newFormula.isfalse() == True:
-            # print("Unit False")
-            return False, newFormula.model, branch
-        unit = newFormula.unit()
+    # loop in case pure literals create units
+    changed = True
+    while changed:
+        changed = False
 
-    # check for pure literals
-    pure = newFormula.pure()
-    while pure != None:
-        # print("pure", pure)
-        newFormula.assign(pure)
-        if newFormula.istrue() == True:
-            # print("Pure True")
-            return True, formula.model, branch
-        elif newFormula.isfalse() == True:
-            # print("Pure False")
-            return False, newFormula.model, branch
-        pure = newFormula.pure()
+        # check for units
+        unit = formula.unit()
+        while unit != None:
+            # print("unit", unit)
+            formula.assign(unit)
+            changed = True
+            if formula.istrue() == True:
+                # print("Unit True")
+                return True, formula.model, branch
+            elif formula.isfalse() == True:
+                # print("Unit False")
+                return False, formula.model, branch
+            unit = formula.unit()
+
+        # check for pure literals
+        pure = formula.pure()
+        while pure != None:
+            # print("pure", pure)
+            formula.assign(pure)
+            changed = True
+            if formula.istrue() == True:
+                # print("Pure True")
+                return True, formula.model, branch
+            elif formula.isfalse() == True:
+                # print("Pure False")
+                return False, formula.model, branch
+            pure = formula.pure()
 
     # check for tautology and contradiction
-    if newFormula.istrue() == True:
+    if formula.istrue() == True:
         # print("Taut True")
-        return True, newFormula.model, branch
-    elif newFormula.isfalse() == True:
+        return True, formula.model, branch
+    elif formula.isfalse() == True:
         # print("Contra False")
-        return False, newFormula.model, branch
+        return False, formula.model, branch
 
-    # if any changes were made to newFormula, they get copied here
-    # formula.model = newFormula.model.copy()
-    # formula.clauses = newFormula.clauses.copy()
+    # if any changes were made to formula, they get copied here
+    # formula.model = formula.model.copy()
+    # formula.clauses = formula.clauses.copy()
 
     # heuristic search
-    # print("Clauses", newFormula.clauses)
-    max = {}
+    # print("Clauses", formula.clauses)
+    if len(formula.clauses) != 0:
 
-    if len(newFormula.clauses) != 0:
-        clauseLen = [len(c) for c in newFormula.clauses]
-        clauseLen = sorted(clauseLen)
-        minLen = clauseLen[0]
+        minLen = min(len(c) for c in formula.clauses)
         # lengths = {}
 
         # creates a dictionary of all lengths
@@ -85,85 +83,78 @@ def theLoop(formula, branch):
         # for l in lengths.keys():
         #     minLen = l
 
-        for n in range(1, newFormula.n+1):
-            max[n] = 0
-            max[-n] = 0
-            for c in newFormula.clauses:
-                if len(c) == minLen:
-                    for i in range(len(c)):
-                        if c[i] == n:
-                            max[n] += 1
-                        elif c[i] == -n:
-                            max[-n] += 1
+        maxVar = {}
+        for c in formula.clauses:
+            if len(c) == minLen:
+                for var in c:
+                    maxVar[var] = maxVar.get(var, 0) + 1
 
-        # sorts to find the most common var
-        maxTuples = [(max[i], i) for i in max.keys()]
-        maxTuples.sort(reverse=True)
 
         # print()
-        # print("max", maxTuples)
+        # print("max", maxVar)
         # print()
 
         # if branch % 25 == 0 and branch != 0:
         #     print(branch)
         #     return None
 
-        # assigns the vars by most common
-        for x in range(len(maxTuples)):
-            # checks the var is actually still in a clause
-            if maxTuples[x][0] != 0:
-                branch += 1
+        # checks the var is actually still in a clause
+        if maxVar != {}:
+            branch += 1
+            chosen = max(maxVar, key=maxVar.get)
+            formula.assign(chosen)
+            # print("chosen", chosen, "model", formula.model)
 
-                chosen = maxTuples[x][1]
-                newFormula.assign(chosen)
-                # print("chosen", chosen, "model", newFormula.model)
+            # tempFormula = Formula.Formula()
+            # tempFormula.clauses = formula.clauses.copy()
+            # tempFormula.model = formula.model.copy()
+            # tempFormula.n = formula.n
+            # tempFormula.m = formula.m
+            # tempFormula.assign(chosen)
 
-                # tempFormula = Formula.Formula()
-                # tempFormula.clauses = newFormula.clauses.copy()
-                # tempFormula.model = newFormula.model.copy()
-                # tempFormula.n = formula.n
-                # tempFormula.m = formula.m
-                # tempFormula.assign(chosen)
+            savedClauses = [c[:] for c in formula.clauses]
+            savedModel = dict(formula.model)
 
+            # print("clauses", tempFormula.clauses)
+            # repeat! first with given result
+            result, solnModel, finalCount = theLoop(formula, branch)
 
-                    # print("clauses", tempFormula.clauses)
-                # repeat! first with given result
-                result, solnModel, finalCount = theLoop(newFormula, branch)
+            # print("branch " + str(branch))
+            # print("solnModel", solnModel)
+            if result == True:
+                # print("Result True")
+                return True, solnModel, finalCount
 
-                # print("branch " + str(branch))
-                # print("solnModel", solnModel)
-                if result == True:
-                    # print("Result True")
-                    return True, solnModel, finalCount
+            # test, opposite result? but undo result
+            formula.model = savedModel
+            formula.clauses = savedClauses
 
-                # test, opposite result? but undo result
-                newFormula.model = formula.model.copy()
-                newFormula.clauses = formula.clauses.copy()
-                newFormula.assign(-chosen)
-                result, solnModel, finalCount = theLoop(newFormula, branch)
-                # print(solnModel)
-                if result == True:
-                    # print("Result True")
-                    return True, solnModel, finalCount
+            formula.assign(-chosen)
+            result, solnModel, finalCount2 = theLoop(formula, branch)
+            # print(solnModel)
+            if result == True:
+                # print("Result True")
+                return True, solnModel, finalCount2
 
-                # undo assignments, need to do this anymore with the way formula is handled?
-                newFormula.model = formula.model.copy()
-                newFormula.clauses = formula.clauses.copy()
-
-                # print("newFormula", newFormula.model,"Formula", formula.model)
-                # print()
+            # print("formula", formula.model,"Formula", formula.model)
+            # print()
 
     # print("End Tuple False")
-    return False, formula.model, branch
+    return False, formula.model, finalCount2
 
 # for printing the results of the file
-def printSoln(formula, model, boolean, branch):
+def printSoln(formula, model, boolean, branch, givenFile):
     # creating a string for naming a file after the current time
     now = datetime.datetime.now()
     now_string = str(now.year) + "-" + str(now.month) + "-" + str(now.day) + " " + str(now.hour) + "-" + str(now.minute)
 
+    if givenFile.find("/") != -1:
+        givenFile = givenFile.split("/",1)[1]
+    if givenFile.find(".") != -1:
+        givenFile = givenFile.split(".",1)[0]
+
     # creating a file
-    filename = f"Solution for CNF on {formula.n} Vars of {formula.m} Clauses - {now_string} .txt"
+    filename = f"Solution for {givenFile} - {now_string}.txt"
     solnFile = open(filename, "w")
 
     # print(model)
@@ -203,10 +194,10 @@ if __name__ == "__main__":
     statement, model, branch = theLoop(formula, branch)
     # print("passed model, end", model)
     if statement == True:
-        printSoln(formula, model, True, branch)
+        printSoln(formula, model, True, branch, sys.argv[1])
         print(statement)
     elif statement == False:
-        printSoln(formula, model, False, branch)
+        printSoln(formula, model, False, branch, sys.argv[1])
         print(statement)
         
         
